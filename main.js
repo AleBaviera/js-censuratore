65fa0ed59c8dc3b4118e6688b533f32d993b77f7
alebaviera <baviera.alessandra@gmailcom>
2019-07-11 14:22:34 +0200

    bozza struttura

---
+++ main.js
@@ -0,0 +1,47 @@
+// CENSURATORE
+// - Il software riceve in input un lungo testo e una serie di parole da censurare.
+// - Restituisce il testo con xxx al posto delle parole censurate.
+// - Stampa un “badword index” calcolato come il numero di parole
+// censurate su il numero di parole totali
+//
+// *NOTE=* il nostro censuratore, diventa una funzione che accetterà
+// due argomenti:   “testo” e “paroleproibite”.
+// se non vi viene immediato il farlo con una funzione, prima fate
+// funzionare lo script e poi vi preoccupate della questione “farlo diventare una funzione!
+
+
+
+// dichiaro var - array testo e array paroleproibite
+var testo = [];
+var paroleproibite = ['serie', 'di', 'parole', 'da', 'censurare'];
+
+// imposto ricerca parole con confronto elementi array
+
+
+// per ogni elemento di testo
+      //
+
+      // per ogni elemento di paroleproibite
+      //  se ==
+      //   stampa xxx
+      //
+      //
+      //  altrimenti
+      //     stampa elemento di testo
+
+
+for (var i = 0; i < testo.length; i++) {
+
+  for (var j = 0; i < paroleproibite.length; i++) {
+    paroleproibite[j]
+
+
+
+
+
+
+    testo[i]
+
+
+  }
+}
